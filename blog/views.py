9a2a6c54eb49f2a5756f688599bdd371545a9ff2
Jorge <jorgeitch@hotmail.com>
2018-10-25 14:44:52 -0500

    añadiendo bootstrap a la pantilla

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,8 @@ from django.utils import timezone
 
 def post_list(request):
 
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.all().order_by('published_date')
 	context = {"posts": posts}
 	return render(request, "post_list.html", context)
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -79,5 +79,5 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-STATIC_URL = '/static/'
+STATIC_URL = '/blog/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
