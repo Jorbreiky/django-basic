e625d2ffb13e126d8edbdd7fb6ee91a6c0abc669
Jorge <jorgeitch@hotmail.com>
2018-10-25 13:53:17 -0500

    Agregando los datos del modelo a la plantilla

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 import datetime
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'post_list.html', {})
+
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	context = {"posts": posts}
+	return render(request, "post_list.html", context)
 
 
 def post_index(request):
