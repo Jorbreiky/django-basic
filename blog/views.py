771434fcbe544df8d502962026708052aa646be1
Jorge <jorgeitch@hotmail.com>
2018-10-25 12:02:54 -0500

    cambio de codigo html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'post_list.html', {})
+
+
+def post_index(request):
+    html = "Hola Mundo"
+    return HttpResponse(html)
