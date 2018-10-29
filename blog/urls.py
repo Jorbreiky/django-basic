8677c7611ec8925c355121605c3adc1271605011
Jorge <jorgeitch@hotmail.com>
2018-10-29 12:43:32 -0600

    añadiendo plantilla bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login, name='login'),
+
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    path('page_not_found/', views.page_not_found, name='page_not_found'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,12 @@ from .models import Post
 import datetime
 from django.utils import timezone
 
+def login(request):
+	return render(request, 'login.html', {})
+
+def page_not_found(request):
+	return render(request, 'page_not_found.html', {})
+
 def post_list(request):
 	#posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	posts = Post.objects.all().order_by('published_date')
@@ -13,7 +19,6 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'post.html', {'post': post})
-
 
 def post_index(request):
     html = "Hola Mundo"
